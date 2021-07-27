160ceb58ba964aa918c2d31ccb30b2c55702cc36
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2021-07-27 14:16:53 +0000

    adcionando comentários

--- mysite/tdc2021_app.py
+++ mysite/tdc2021_app.py
@@ -1,5 +1,12 @@
-
-# A very simple Flask Hello World app for you to get started with...
+#
+# Aplicação criada para fins de exemplo
+# Para: TDC 2021 - Transformation
+#
+# Trilha: Python
+#
+# Autores:  Arnott Ramos Caiado
+#           Inoã Liberato
+#
 
 from flask import Flask
 from flask import render_template
@@ -7,14 +14,17 @@ import tdc2021_func
 
 app = Flask(__name__)
 
+# endpoint http://tdc2021.pythonanywhere.com/
 @app.route('/')
 def hello_world():
     return 'Ola. Minha primeira aplicacao'
 
+# endpoint http://tdc2021.pythonanywhere.com/tdc
 @app.route('/tdc')
 def tdc_msg():
     return render_template( "index.html" )
 
+# endpoint http://tdc2021.pythonanywhere.com/tdc/api
 @app.route('/tdc/api')
 def tdc_api():
     return {"Api":"exemplo"}
