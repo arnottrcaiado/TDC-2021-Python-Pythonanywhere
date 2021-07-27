008b5a5b3b5c086a3dcdd04518c95ad121645559
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2021-07-27 14:29:02 +0000

    adcionando comentários no arquivo de funcoes

--- mysite/tdc2021_func.py
+++ mysite/tdc2021_func.py
@@ -1,3 +1,13 @@
-# funcoes acessorias
-
+#
+# Aplicação criada para fins de exemplo
+# Para: TDC 2021 - Transformation
+#
+# Trilha: Python
+#
+# Autores:  Arnott Ramos Caiado
+#           Inoã Liberato
+#
+# ------------------------------------------------
+# funcoes de apoio e acessorias
+# ----------------------------------------------------------------------------
 chave_antenticacao = 'aSdrt12677489IopkljgrtrewwfghvcbnMmjgsfwety553724132788'
