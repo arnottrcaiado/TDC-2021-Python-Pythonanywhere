ea275c196ae9bf1fd88630a39277f108fea924a5
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2021-08-02 14:55:44 +0000

    add telas html basicas

--- mysite/tdc2021_app.py
+++ mysite/tdc2021_app.py
@@ -21,16 +21,19 @@ import tdc2021_func
 
 app = Flask(__name__)
 
+#---------------------------------------------
 # endpoint http://tdc2021.pythonanywhere.com/
 @app.route('/')
 def hello_world():
     return 'Ola TDC 2021. Minha primeira aplicacao'
 
+#--------------------------------------------------
 # endpoint http://tdc2021.pythonanywhere.com/tdc
 @app.route('/tdc')
 def tdc_msg():
     return render_template( "index.html" )
 
+#-----------------------------------------------------
 # endpoint http://tdc2021.pythonanywhere.com/tdc/api
 @app.route('/tdc/api')
 def tdc_api():
